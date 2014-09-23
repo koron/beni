302353f9c46d5a5005da395982ccad09cd56d16d
MURAOKA Taro <koron.kaoriya@gmail.com>
2014-09-24 02:44:55 +0900

    library skeleton.  make as utility

---
+++ Makefile
@@ -0,0 +1,11 @@
+SUBDIRS = ./token ./lexer ./theme ./formatter . ./cmd/beni
+
+test:
+	go test $(SUBDIRS)
+
+tags:
+	ctags -R --exclude=tmp .
+
+clean:
+	rm -f beni beni.exe
+	rm -f tags
---
+++ beni.go
@@ -0,0 +1,9 @@
+package beni
+
+import "io"
+
+// Highlight convert source code to syntax highlighted.
+func Highlight(r io.Reader, w io.Writer, lexer, theme, format string) error {
+	// TODO:
+	return nil
+}
